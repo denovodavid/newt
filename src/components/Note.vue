63d449d5684812bce7513c476244b35f1cdef78a
David Holland <David Holland>
2016-11-09 23:45:25 +1100

    Add events and styles to note

--- src/components/Note.vue
+++ src/components/Note.vue
@@ -4,9 +4,9 @@
       <div class="right floated meta drag-handle" style="visibility: hidden;">
         <i class="block layout icon"></i>
       </div>
-      <div class="header">note.title</div>
+      <div class="header">{{ note.title }}</div>
       <div class="description">
-        <p style="white-space: pre-wrap; word-wrap: break-word; text-overflow: ellipsis;">note.text</p>
+        <p style="white-space: pre-wrap; word-wrap: break-word; text-overflow: ellipsis;">{{ note.text }}</p>
       </div>
       <div></div>
     </div>
@@ -26,7 +26,7 @@
         </div>
       </span>
       <span class="right floated">
-      <span>note.created_at</span>
+      <span>{{ note.created_at }}</span>
       <div class="ui icon dropdown">
         <i class="icon ellipsis vertical"></i>
         <div class="menu">
@@ -41,14 +41,44 @@
 </template>
 
 <script>
+import $ from 'jquery'
+
 export default {
   name: 'note',
-  data () {
-    return {
-      msg: 'lol'
-    }
+  props: ['note'],
+  mounted () {
+    var self = this
+    var $note = $(self.$el)
+    $note.on({
+      mouseenter: function () {
+        $(this).find('.extra.content').css({'visibility': 'visible'})
+        $(this).find('.drag-handle').css({'visibility': 'visible'})
+      },
+      mouseleave: function () {
+        $(this).find('.extra.content').css({'visibility': 'hidden'})
+        $(this).find('.drag-handle').css({'visibility': 'hidden'})
+      },
+      click: function () {
+        $(self.$parent.$el).children('.newt-note').css('z-index', '')
+        $(this).css('z-index', '2')
+      }
+    });
+    self.$parent.shapeshift()
   }
 }
 </script>
 
-<style></style>
+<style scoped>
+  .drag-handle {
+    cursor: move;
+  }
+  
+  .ss-placeholder-child {
+    border: 1px dashed #999;
+    border-radius: 4px;
+    position: absolute;
+    transition: left 0.4s ease, top 0.4s ease;
+    width: 290px;
+    z-index: 0;
+  }
+</style>
