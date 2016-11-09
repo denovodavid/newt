3f1dba3f447f0abfc3498cecd00d79261e014b87
David Holland <David Holland>
2016-11-09 23:46:09 +1100

    Add db ref and styles to notes

--- src/components/Notes.vue
+++ src/components/Notes.vue
@@ -1,28 +1,64 @@
 <template>
   <div class="newt-notes">
     <div class="newt-note" v-for="note in notes">
-      <note></note>
+      <note v-bind:note="note"></note>
     </div>
   </div>
 </template>
 
 <script>
 import Note from './Note'
+import $ from 'jquery'
+
+var shapeshiftOptions = {
+  selector: '.newt-note',
+  colWidth: 290,
+  animated: false,
+  handle: '.drag-handle'
+};
 
 export default {
   name: 'notes',
-  data () {
+  firebase () {
     return {
-      notes: [
-        'note1'
-      ],
-      msg: 'lol'
+      notes: this.$root.db.ref('notes')
     }
   },
   components: {
     Note
+  },
+  mounted () {
+    var self = this
+    var $notes = $(self.$el)
+    $notes.on('ss-drop-complete', (e) => {
+      console.log('setNotesOrder')
+      // self.vm.setNotesOrder();
+    })
+
+    console.log(self)
+  },
+  methods: {
+    shapeshift () {
+      $(this.$el).shapeshift(shapeshiftOptions)
+      console.log('SHAPESHIPT')
+    }
   }
 }
 </script>
 
-<style></style>
+<style scoped>
+  .newt-notes {
+    position: relative;
+  }
+  
+  .newt-note {
+    background: #CCC;
+    position: absolute;
+    transition: left 0.4s ease, top 0.4s ease;
+    z-index: 1;
+  }
+  
+  .ui-draggable-dragging {
+    transition: none;
+  }
+</style>
