7344412a345ead326b54ae74ef60e361af1d03f7
David Holland <David Holland>
2016-11-09 23:43:00 +1100

    Use vuefire in module

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 import Vue from 'vue'
+import VueFire from 'vuefire'
 import App from './App'
+import db from './firebaseDatabase'
+
+Vue.use(VueFire)
+
+console.log('NEWT!')
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   template: '<App/>',
-  components: { App }
+  components: { App },
+  data: {
+    db: db
+  }
 });
