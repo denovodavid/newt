ae855f4b78568386918ea8d08e26184a1d483ab0
David Holland <David Holland>
2016-11-10 23:03:06 +1100

    Add colors

--- src/main.js
+++ src/main.js
@@ -27,6 +27,18 @@ new Vue({
   template: '<App/>',
   components: { App },
   data: {
-    db: db
+    db: db,
+    colors: {
+      none: '',
+      red: '#ff8a80',
+      orange: '#ffd180',
+      yellow: '#ffff8d',
+      green: '#ccff90',
+      teal: '#a7ffeb',
+      blue: '#80d8ff',
+      purple: '#b680ff',
+      pink: '#ff80ef',
+      grey: '#cfd8dc'
+    }
   }
 });
