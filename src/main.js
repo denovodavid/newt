5022f789b132fe0acb079a9299cdd5ed33bfcd56
David Holland <David Holland>
2016-11-10 18:00:56 +1100

    Import dependencies

--- src/main.js
+++ src/main.js
@@ -3,6 +3,20 @@ import VueFire from 'vuefire'
 import App from './App'
 import db from './firebaseDatabase'
 
+import 'jquery-ui/jquery-ui.min.js'
+
+import 'jquery.ns-autogrow'
+
+import imagesLoaded from 'imagesloaded'
+imagesLoaded.makeJQueryPlugin($)
+
+import 'semantic-ui-css/semantic.js'
+import 'semantic-ui-css/semantic.css'
+
+import marked from 'marked'
+
+import 'jquery-shapeshift'
+
 Vue.use(VueFire)
 
 console.log('NEWT!')
