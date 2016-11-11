7c849db425c785b27b7df91a06c6d29fed77752c
David Holland <David Holland>
2016-11-11 11:22:13 +1100

    Add eslint and e2e tests

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -34,6 +34,20 @@ module.exports = {
     fallback: [path.join(__dirname, '../node_modules')]
   },
   module: {
+    preLoaders: [
+      {
+        test: /\.vue$/,
+        loader: 'eslint',
+        include: projectRoot,
+        exclude: /node_modules/
+      },
+      {
+        test: /\.js$/,
+        loader: 'eslint',
+        include: projectRoot,
+        exclude: /node_modules/
+      }
+    ],
     loaders: [
       {
         test: /\.vue$/,
@@ -66,6 +80,9 @@ module.exports = {
         }
       }
     ]
+  },
+  eslint: {
+    formatter: require('eslint-friendly-formatter')
   },
   plugins: [
     new webpack.ProvidePlugin({
