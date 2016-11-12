ef493761ce13b359f3c722c77fbbd42b11850f82
David Holland <David Holland>
2016-11-12 21:08:05 +1100

    Add global formatDate filter

--- src/main.js
+++ src/main.js
@@ -36,3 +36,8 @@ Vue.directive('dropdown', {
 Vue.filter('capatalise', (text) => {
   return text[0].toUpperCase() + text.slice(1)
 })
+
+Vue.filter('formatDate', (value) => {
+  var date = new Date(value)
+  return date.getDate() + '/' + date.getMonth() + '/' + date.getFullYear()
+})
