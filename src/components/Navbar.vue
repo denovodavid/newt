f7fb7d8128bafabfa234e65e626c03855c67769f
David J Holland <stooge213@hotmail.com>
2016-12-21 13:16:25 +1100

    Remove useless nav container

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="ui fluid container">
-    <div class="ui yellow inverted borderless menu" style="border-radius: 0;">
-      <div class="grey item" style="color: #222 !important;">
-        <h3>Newt</h3>
-      </div>
-      <a class="item" style="color: #222 !important;">Vue</a>
-      <!-- <a class="item disabled" style="color: #222 !important; text-decoration: line-through;">Angular</a>
-      <a class="item disabled" style="color: #222 !important; text-decoration: line-through;">React</a>
-      <a class="item disabled" style="color: #222 !important; text-decoration: line-through;">Ember</a>
-      <a class="item disabled" style="color: #222 !important; text-decoration: line-through;">Meteor</a> -->
-      <div class="right menu">
-        <div class="ui item">
-          <i id="loading-icon" class="icon large checkmark" style="color: #222 !important;"></i>
-        </div>
+  <div class="ui yellow inverted borderless menu" style="border-radius: 0;">
+    <div class="grey item" style="color: #222 !important;">
+      <h3>Newt</h3>
+    </div>
+    <a class="item" style="color: #222 !important;">Vue</a>
+    <!-- <a class="item disabled" style="color: #222 !important; text-decoration: line-through;">Angular</a>
+    <a class="item disabled" style="color: #222 !important; text-decoration: line-through;">React</a>
+    <a class="item disabled" style="color: #222 !important; text-decoration: line-through;">Ember</a>
+    <a class="item disabled" style="color: #222 !important; text-decoration: line-through;">Meteor</a> -->
+    <div class="right menu">
+      <div class="ui item">
+        <i id="loading-icon" class="icon large checkmark" style="color: #222 !important;"></i>
       </div>
     </div>
   </div>
