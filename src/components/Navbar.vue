8b2f5e41d578113670a415fa5fef0c850cdc5673
David J Holland <stooge213@hotmail.com>
2016-12-24 11:09:39 +1100

    Comment out unused items

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,16 +3,16 @@
     <div class="grey item" style="color: #222 !important;">
       <h3>Newt</h3>
     </div>
-    <a class="item" style="color: #222 !important;">Vue</a>
+    <!--<a class="item" style="color: #222 !important;">Vue</a>-->
     <!-- <a class="item disabled" style="color: #222 !important; text-decoration: line-through;">Angular</a>
     <a class="item disabled" style="color: #222 !important; text-decoration: line-through;">React</a>
     <a class="item disabled" style="color: #222 !important; text-decoration: line-through;">Ember</a>
     <a class="item disabled" style="color: #222 !important; text-decoration: line-through;">Meteor</a> -->
-    <div class="right menu">
+    <!--<div class="right menu">
       <div class="ui item">
         <i id="loading-icon" class="icon large checkmark" style="color: #222 !important;"></i>
       </div>
-    </div>
+    </div>-->
   </div>
 </template>
 
