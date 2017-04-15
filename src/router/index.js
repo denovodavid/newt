0ceb0826587091b336825f826c86a4f25a0b6370
David J Holland <stooge213@hotmail.com>
2017-04-15 10:21:13 +0100

    Super Basic Authentication

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 // import Hello from '@/components/Hello'
+import Signin from '@/components/SignIn'
 import Newt from '@/components/Newt'
 
 Vue.use(Router)
@@ -9,8 +10,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Newt',
+      name: 'home',
       component: Newt
+    },
+    {
+      path: '/signin',
+      name: 'signin',
+      component: Signin
     }
   ]
 })
