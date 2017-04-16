c1d7f398ca5b56dd0cbcc7ca60454a5ec1fded2e
David J Holland <stooge213@hotmail.com>
2017-04-16 23:11:07 +0100

    Add babel-polyfill for PhantomJS

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -14,7 +14,7 @@ module.exports = function (config) {
     browsers: ['PhantomJS'],
     frameworks: ['mocha', 'sinon-chai', 'phantomjs-shim'],
     reporters: ['spec', 'coverage'],
-    files: ['./index.js'],
+    files: ['../../node_modules/babel-polyfill/dist/polyfill.min.js', './index.js'],
     preprocessors: {
       './index.js': ['webpack', 'sourcemap']
     },
