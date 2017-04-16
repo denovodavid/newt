9bf4eead36e967bbdfa762c70aa4ea00918ba81e
David J Holland <stooge213@hotmail.com>
2017-04-16 20:16:25 +0100

    Return copy of notes sorted

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,9 +1,9 @@
 import Marked from 'marked'
 
 export const orderedNotes = state => {
-  return state.notes.sort((a, b) => {
-    var aOrder = state.notesOrder[a['.key']]
-    var bOrder = state.notesOrder[b['.key']]
+  return state.notes.slice(0).sort((a, b) => {
+    const aOrder = state.notesOrder[a['.key']]
+    const bOrder = state.notesOrder[b['.key']]
     if (aOrder === undefined || aOrder === null) return -1
     if (aOrder === bOrder) return 0
     if (aOrder < bOrder) return -1
