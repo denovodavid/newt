dec60399f75b1560575c758177690cb2890a1dc8
David J Holland <stooge213@hotmail.com>
2017-04-20 21:16:08 +0100

    New sign in page

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -1,27 +1,64 @@
 <template>
-  <div id="signin">
-    <form id="signin-form" class="ui form" v-on:submit.prevent="">
-      <div class="ui input">
-        <input type="email" placeholder="Email" v-model="login.email">
+  <div class="ui center aligned text container sign-in-container">
+    <h1 class="ui center aligned grey icon header">
+      <i class="sticky note yellow icon"></i>
+      NEWT
+      <div class="sub header">dem notes yo</div>
+    </h1>
+    <form class="ui form" @submit.prevent="toggleSignIn" :class="{ loading: isLoading }">
+      <div class="field">
+        <div class="ui left icon input">
+          <input type="email" name="email" placeholder="Email" v-model="login.email">
+          <i class="user icon"></i>
+        </div>
       </div>
-      <br><br>
-      <div class="ui input">
-        <input type="password" placeholder="Password" v-model="login.password">
+      <div class="field">
+        <div class="ui left icon input">
+          <input type="password" name="password" placeholder="Password" v-model="login.password">
+          <i class="lock icon"></i>
+        </div>
       </div>
-      <br><br>
-      <button class="ui button" v-on:click="toggleSignIn()">Sign In</button>
-    </form>
-    <br><br>
-    <form id="signup-form" class="ui form" v-on:submit.prevent="">
-      <div class="ui input">
-        <input type="email" placeholder="Email" v-model="signup.email">
+      <div class="field">
+        <button class="ui grey large animated fade button" type="submit">
+          <div class="visible content">Sign in</div>
+          <div class="hidden content">
+            <i class="sign in icon"></i>
+          </div>
+        </button>
       </div>
-      <br><br>
-      <div class="ui input">
-        <input type="password" placeholder="Password" v-model="signup.password">
+      <p><small>(Coming soon) Sign in with...</small></p>
+      <div class="ui stackable centered four column grid">
+        <div class="column">
+          <button type="button" class="ui google plus large fluid button disabled">
+            <i class="google icon"></i>
+            Google
+          </button>
+        </div>
+        <div class="column">
+          <button type="button" class="ui facebook large fluid button disabled">
+            <i class="facebook icon"></i>
+            Facebook
+          </button>
+        </div>
+        <div class="column">
+          <button type="button" class="ui twitter large fluid button disabled">
+            <i class="twitter icon"></i>
+            Twitter
+          </button>
+        </div>
+        <div class="column">
+          <button type="button" class="ui black large fluid button disabled">
+            <i class="github icon"></i>
+            Github
+          </button>
+        </div>
+        <div class="sixteen wide center aligned column">
+          <button type="button" class="ui large basic button disabled">
+            <i class="mail icon"></i>
+            Sign up with email and password
+          </button>
+        </div>
       </div>
-      <br><br>
-      <button class="ui button" v-on:click="handleSignUp()">Sign Up</button>
     </form>
   </div>
 </template>
@@ -29,6 +66,7 @@
 <script>
 import router from '../router'
 import { firebaseApp } from '../firebase'
+import { mapActions } from 'vuex'
 
 export default {
   name: 'signin',
@@ -38,21 +76,17 @@ export default {
         email: '',
         password: ''
       },
-      signup: {
-        email: '',
-        password: ''
-      }
+      isLoading: false
     }
   },
   methods: {
     toggleSignIn () {
+      const self = this
       if (firebaseApp.auth().currentUser) {
-        // [START signout]
         firebaseApp.auth().signOut()
-        // [END signout]
       } else {
-        var email = this.login.email
-        var password = this.login.password
+        var email = self.login.email
+        var password = self.login.password
         if (email.length < 4) {
           alert('Please enter an email address.')
           return
@@ -61,90 +95,48 @@ export default {
           alert('Please enter a password.')
           return
         }
-        // Sign in with email and pass.
-        // [START authwithemail]
-        firebaseApp.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
-          // Handle Errors here.
+        this.signInWithEmailAndPassword({
+          email,
+          password
+        }).catch(error => {
           var errorCode = error.code
           var errorMessage = error.message
-          // [START_EXCLUDE]
           if (errorCode === 'auth/wrong-password') {
             alert('Wrong password.')
           } else {
             alert(errorMessage)
           }
           console.log(error)
-          // document.getElementById('quickstart-sign-in').disabled = false
-          // [END_EXCLUDE]
+          self.isLoading = false
         })
-        // [END authwithemail]
+        self.isLoading = true
       }
-      // document.getElementById('quickstart-sign-in').disabled = true
     },
-    handleSignUp () {
-      var email = this.signup.email
-      var password = this.signup.password
-      if (email.length < 4) {
-        alert('Please enter an email address.')
-        return
-      }
-      if (password.length < 4) {
-        alert('Please enter a password.')
-        return
-      }
-      // Sign in with email and pass.
-      // [START createwithemail]
-      firebaseApp.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
-        // Handle Errors here.
-        var errorCode = error.code
-        var errorMessage = error.message
-        // [START_EXCLUDE]
-        if (errorCode === 'auth/weak-password') {
-          alert('The password is too weak.')
-        } else {
-          alert(errorMessage)
-        }
-        console.log(error)
-        // [END_EXCLUDE]
-      })
-      // [END createwithemail]
-    }
+    ...mapActions([
+      'signInWithEmailAndPassword'
+    ])
   }
 }
 
 firebaseApp.auth().onAuthStateChanged(function (user) {
   if (user) {
-    // User is signed in.
-    // var displayName = user.displayName
-    // var email = user.email
-    // var emailVerified = user.emailVerified
-    // var photoURL = user.photoURL
-    // var isAnonymous = user.isAnonymous
-    // var uid = user.uid
-    // var providerData = user.providerData
-    // ...
     console.log('LOGGED IN!')
     router.push({name: 'home'})
-  } else {
-    console.log('User is signed out!')
-    // User is signed out.
-    // ...
   }
 })
 </script>
 
-<style>
-body {
-  background: #eee;
-  height: auto !important;
+<style scoped>
+.ui.form .field {
+  max-width: 20em;
+  margin: auto auto 1em;
 }
 
-#signin {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.sign-in-container {
+  padding-top: 3em;
+}
+
+.ui.loading.form:before {
+  background: rgba(249, 249, 249, 0.8);
 }
 </style>
