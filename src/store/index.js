977939d8131c5d03df9817e01ef8533ea2715b7f
David J Holland <stooge213@hotmail.com>
2017-04-20 18:24:47 +0100

    Move state to module

--- src/store/index.js
+++ src/store/index.js
@@ -1,28 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-// import { firebaseMutations } from 'vuexfire'
+import state from './state'
 import * as getters from './getters'
 import * as actions from './actions'
 import mutations from './mutations'
-import colors from '../colors'
 
 Vue.use(Vuex)
-
-const state = {
-  notes: [],
-  notesOrder: {},
-  editor: {
-    show: false,
-    note: {}
-  },
-  newNote: {
-    title: '',
-    text: '',
-    markdown: false,
-    color: 'none'
-  },
-  colors
-}
 
 export const options = {
   state,
---
+++ src/store/state.js
@@ -0,0 +1,43 @@
+import colors from '../colors'
+
+export default {
+  notes: [
+    /*
+    {
+      '.key': ''
+      checked: false,
+      color: '',
+      created_at: '',
+      markdown: false,
+      text: '',
+      title: ''
+    }
+    */
+  ],
+  notesOrder: {
+    /*
+    'noteKey': 1
+    */
+  },
+  editor: {
+    show: false,
+    note: {
+      /*
+      '.key': ''
+      checked: false,
+      color: '',
+      created_at: '',
+      markdown: false,
+      text: '',
+      title: ''
+      */
+    }
+  },
+  newNote: {
+    title: '',
+    text: '',
+    markdown: false,
+    color: 'none'
+  },
+  colors
+}
