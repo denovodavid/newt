6bb48c572c4c05b3d4d91d45e5d7c47aa0fc3e0c
David J Holland <stooge213@hotmail.com>
2017-04-21 12:40:43 +0100

    Remove vue date filter

--- src/filters.js
+++ src/filters.js
@@ -3,8 +3,3 @@ import Vue from 'vue'
 Vue.filter('capitalise', (text) => {
   return text[0].toUpperCase() + text.slice(1)
 })
-
-Vue.filter('formatDate', (value) => {
-  var date = new Date(value)
-  return date.getDate() + '/' + date.getMonth() + '/' + date.getFullYear()
-})
