6d4783a548a189f83d7da185956b680c2d37fc40
David J Holland <stooge213@hotmail.com>
2017-04-30 10:14:29 +0100

    transition spinner

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,8 @@
 <template>
-<div class="ui yellow inverted borderless menu" style="border-radius: 0;">
-  <div class="grey item" style="color: #222 !important;">
+<div class="ui yellow inverted borderless menu"
+     style="border-radius: 0;">
+  <div class="grey item"
+       style="color: #222 !important;">
     <h3>Newt</h3>
   </div>
   <!--<a class="item" style="color: #222 !important;">Vue</a>-->
@@ -9,10 +11,23 @@
     <a class="item disabled" style="color: #222 !important; text-decoration: line-through;">Ember</a>
     <a class="item disabled" style="color: #222 !important; text-decoration: line-through;">Meteor</a> -->
   <div class="right menu">
-      <a class="item" v-on:click="handleSignOut()">Sign Out</a>
-      <div class="ui item">
-        <i id="loading-icon" :class="isLoading" class="icon large" style="color: #222 !important;"></i>
-      </div>
+      <a class="item"
+         @click="handleSignOut()">Sign Out</a>
+      <transition name="fade"
+                  mode="out-in">
+        <div class="ui item"
+             style="color: #222 !important;"
+             v-if="asyncLoading"
+             :key="asyncLoading">
+          <i class="notched circle loading icon large"></i>
+        </div>
+        <div class="ui item"
+             style="color: #222 !important;"
+             v-else
+             :key="asyncLoading">
+          <i class="checkmark icon large"></i>
+        </div>
+      </transition>
     </div>
 </div>
 </template>
@@ -24,12 +39,6 @@ import { firebaseApp } from '../firebase'
 export default {
   name: 'navbar',
   computed: {
-    isLoading () {
-      return {
-        checkmark: !this.asyncLoading,
-        'notched circle loading': this.asyncLoading
-      }
-    },
     ...mapState([
       'asyncLoading'
     ])
@@ -51,5 +60,13 @@ export default {
 
 .ui.inverted.menu .item:hover {
   color: #000;
+}
+
+.fade-enter-active.fade-enter-active.fade-enter-active, .fade-leave-active.fade-leave-active.fade-leave-active {
+  transition: opacity 0.25s;
+}
+
+.fade-enter.fade-enter.fade-enter, .fade-leave-to.fade-leave-to.fade-leave-to {
+  opacity: 0;
 }
 </style>
