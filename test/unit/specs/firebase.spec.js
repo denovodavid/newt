eb25c481a0fbb82c685fed8cf6043e16512ec6f1
David J Holland <stooge213@hotmail.com>
2017-06-11 16:11:22 +0100

    Remove useless specs

--- test/unit/specs/firebase.spec.js
+++
@@ -1,7 +0,0 @@
-import { firebaseApp } from '@/firebaseApp'
-
-describe('Firebase', () => {
-  it('should load the config without crashing', () => {
-    expect(firebaseApp).not.toBeUndefined()
-  })
-})
